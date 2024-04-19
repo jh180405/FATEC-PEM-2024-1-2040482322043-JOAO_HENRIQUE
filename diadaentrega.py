b89da6de562cef397fb74c61d325e29bd51543ed
Joao Henrique <jhpds18@gmail.com>
2024-04-19 19:01:47 -0300

    Create diadaentrega.py

---
+++ diadaentrega.py
@@ -0,0 +1,16 @@
+dia = str(input())
+prazo = int(input())
+
+dias = ("domingo", "segunda", "terca", "quarta", "quinta", "sexta", "sabado")
+
+indice = dias.index(dia)
+
+entrega = indice + prazo
+
+if entrega > 6
+   entrega - 6
+
+if prazo == 0:
+    print('chega hoje!')
+else:
+    print('sera entregue', dias[entrega])
